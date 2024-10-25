ab9617ea55995a650d964bbd17bdbd36a2a85d9e
amaan2106 <amaan.ali2106@gmail.com>
2024-10-24 21:51:01 -0400

    Added google API, added components folder which has our features we need, such as crowdhistory (the history of previous crowds), RestaurantCrowd which is the live number of crowd currently, The searchBar which helpsyou search for restaurant, and a map, which helps you see the full picture of restaurants nearby. this is just the structure I've implemented, we still have to style it and implement the funcitonality. however, we are in a much better spot now.

--- CrowdPredictor/src/App.tsx
+++ CrowdPredictor/src/App.tsx
@@ -1,12 +1,26 @@
+// src/App.tsx
+import React, { useState } from 'react';
+import Map from './components/Map';
+import RestaurantCrowd from './components/RestaurantCrowd';
+import SearchBar from './components/SearchBar';
+import CrowdHistory from './components/CrowdHistory';
 
 
 function App() {
+  const [placeId, setPlaceId] = useState(''); // Example placeId
 
-  return(
+  const handleSearch = (query: string) => {
+    // Implement search logic to get placeId
+  };
+
+  return (
     <div>
-      CrowdPredictor
+      <SearchBar onSearch={handleSearch} />
+      <Map />
+      <RestaurantCrowd placeId={placeId} />
+      <CrowdHistory placeId={placeId} />
     </div>
   );
 }
 
-export default App
+export default App;
